f60d4a60b68f16b5415139dd671096e7ed409219
mzhou <zmmbreeze0825@gmail.com>
2013-03-18 23:29:39 +0800

    update

--- template/JSONEditor.js
+++ template/JSONEditor.js
@@ -1,6 +1,13 @@
 
 function JSONEditor($scope) {
     $scope.inputs = [
-        {name: '', note: '', type: '', }
+        {name: 'id', note: 'Number to show', type: 'Number'},
+        {name: 'name', note: 'Your names', type: 'Text'},
+        {name: 'position', note: 'Position', type: 'Float'},
+        {name: '', note: '', type: ''},
+        {name: '', note: '', type: ''},
+        {name: '', note: '', type: ''},
+        {name: '', note: '', type: ''},
+        {name: '', note: '', type: ''}
     ];
 }
